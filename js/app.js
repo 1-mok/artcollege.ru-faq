b4ac0f89c2d642249183bae2e12cef93d121cc92
Golik Vladislav <rozzel@mail.ru>
2022-09-29 13:05:26 +0300

    app

--- js/app.js
+++ js/app.js
@@ -1,2 +1,4 @@
-let zeroMd = getElementsByTagName(zero-md);
-console.log(zeroMd);
+let zeroMd = document.getElementById("zeroMd");
+let dateMd = Date.now();
+
+zeroMd.src = `https://1-mok.github.io/artcollege.ru-faq/faq.md?${dateMd}`;
